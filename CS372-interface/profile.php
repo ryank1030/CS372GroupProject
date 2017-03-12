9e094f647517a3e757002762977792d2fb59e3d9
tballer <mcdivitt@uregina.ca>
2017-03-12 07:23:16 -0600

    Interface
    work in progress for the interface

---
+++ CS372-interface/profile.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Group Share</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+</head>
+
+<body>
+
+ <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="row">
+                
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Group Share</a>
+            </div>
+                
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">    
+                <ul class="nav navbar-nav navbar-right">
+                    
+                    <!-- Group request dropdown -->
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" id="menu1" data-toggle="dropdown">
+                            <i class="fa fa-users" aria-hidden="true"></i> 
+                                Group Requests <span class="badge badge-notify">2</span></a>
+                        <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+                            <li role="presentation"><a role="menuitem" tabindex="-1">Group Name 1
+                                <button class="btn btn-info btn-sm margin-left" type="button">Accept</button>
+                                <button class="btn btn-default btn-sm" type="button">Decline Request</button>
+                            </a></li>
+                            <li role="presentation"><a role="menuitem" tabindex="-1">Group Name 2
+                                <button class="btn btn-info btn-sm margin-left" type="button">Accept</button>
+                                <button class="btn btn-default btn-sm" type="button">Decline Request</button>
+                            </a></li>
+                        </ul>
+                    </li>
+                    <!-- ./group request dropdown -->
+                    
+                    <!-- profile and logout -->
+                    <li><a href="#top"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                    <!-- ./profile logout -->
+                    
+                </ul>
+                
+                <!-- Search Bar -->
+                <div class="col-md-3">
+                    <ul class="nav navbar-nav">
+                        <li id="search-group" class="input-group">
+                            <span class ="input-group-btn">
+                                <input type="text" class="form-control input-sm" placeholder="Search for a group" />
+                                <button class="btn btn-info btn-sm" type="button">
+                                    <i class="glyphicon glyphicon-search"></i>
+                                </button>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <!-- ./search bar -->
+                
+                
+            </div>
+            <!-- /.navbar-collapse -->
+            </div>
+            <!-- /. row -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    
+    <!-- Page Header -->
+    <div class="container">
+        <div class="row">
+        <div class="page-header">
+            <h3><img src="img/img.png" class="img-circle" alt="Profile Picture" width="70" height="70"> Profile</h3>      
+        </div>
+        </div>
+    </div>               
+    
+    <!-- /.container -->
+    
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
