0abdd47e20e128a828dcc2782298fa95bcbf9899
brecur <brecur@gmail.com>
2017-03-14 18:19:56 -0600

    Added script to Remove All Tables
    use this to clear out the entire table structure

---
+++ DataBase files/removeAllTables.sql
@@ -0,0 +1,12 @@
+/* script for clearing the entire table structure */
+
+DROP TABLE Personal_Calendar;
+DROP TABLE Group_Calendar;
+DROP TABLE Chat_Messages;
+DROP TABLE Personal_Calendar;
+DROP TABLE Group_Files;
+DROP TABLE Files;
+DROP TABLE Add_Users_Pending;
+DROP TABLE Group_Users;
+DROP TABLE Groups;
+DROP TABLE Users;
