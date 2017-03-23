9382262ef86e70dd917fcf0f278cc457a42f4881
tballer <mcdivitt@uregina.ca>
2017-03-23 14:35:21 -0600

    removed script at bottom
    removed script at bottom

--- CS372-interface2/group_page.php
+++ CS372-interface2/group_page.php
@@ -543,30 +543,7 @@ else
     
     <!-- Custom JavaScript -->
     <script src="js/custominterface.js"></script>
-    
-    
-    <!-- Date Picker Scripts -->
-    <script type="text/javascript"
-     src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js">
-    </script> 
-    <script type="text/javascript"
-     src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js">
-    </script>
-    <script type="text/javascript"
-     src="http://tarruda.github.com/bootstrap-datetimepicker/assets/js/bootstrap-datetimepicker.min.js">
-    </script>
-    <script type="text/javascript"
-     src="http://tarruda.github.com/bootstrap-datetimepicker/assets/js/bootstrap-datetimepicker.pt-BR.js">
-    </script>
-    <script type="text/javascript">
-      $('#datetimepicker1').datetimepicker({
-        format: 'yyyy/MM/dd hh:mm:ss',
-      });
-      $('#datetimepicker2').datetimepicker({
-        format: 'yyyy/MM/dd hh:mm:ss',
-      });
-        
-    </script>
+
     
     
 
